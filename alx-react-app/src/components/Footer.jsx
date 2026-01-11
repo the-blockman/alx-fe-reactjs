4be072e308af81832d61120eef9ad93a904bf80e
the-blockman <105639638+the-blockman@users.noreply.github.com>
2026-01-11 19:55:13 +0100

    feat: add Header, MainContent, and Footer components to App

---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <p>© 2023 City Lovers</p>;
+}
+
+export default Footer;
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,5 @@
+function MainContent() {
+  return <p>I love to visit New York, Paris, and Tokyo.</p>;
+}
+
+export default MainContent;
