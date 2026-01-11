93b226db0dc0369ab31fe899877377955dc1bb54
the-blockman <105639638+the-blockman@users.noreply.github.com>
2026-01-11 22:04:02 +0100

    feat: add UserProfile component and integrate it into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -35,6 +36,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Justin" age="24" bio="Loves peace of mind" />
       <Footer />
     </>
   );
