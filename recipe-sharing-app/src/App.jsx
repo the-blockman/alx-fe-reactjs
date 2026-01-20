e0ce3d457c27a449332db1418b046eae03a4afd7
the-blockman <105639638+the-blockman@users.noreply.github.com>
2026-01-20 16:34:37 +0100

    add Router keyword

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
@@ -10,19 +10,21 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
 
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-      </Routes>
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
+      </Router>
     </>
   );
 }
