cc581bc0eca86df83f9265adf3f6a842c568c8c5
the-blockman <105639638+the-blockman@users.noreply.github.com>
2026-01-25 16:29:59 +0100

    feat: implement routing and create main components for Home, About, Services, and Contact

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "10px",
+        backgroundColor: "#333",
+        display: "flex",
+        gap: "15px",
+      }}
+    >
+      <Link style={{ color: "#fff" }} to="/">
+        Home
+      </Link>
+      <Link style={{ color: "#fff" }} to="/about">
+        About
+      </Link>
+      <Link style={{ color: "#fff" }} to="/services">
+        Services
+      </Link>
+      <Link style={{ color: "#fff" }} to="/contact">
+        Contact
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
