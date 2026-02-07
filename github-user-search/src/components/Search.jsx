526e52a72676a2141d20c2ba386806aa2df4e695
the-blockman <105639638+the-blockman@users.noreply.github.com>
2026-02-07 08:25:54 +0100

    refactor: rename API service module and update import path in Search component

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { fetchUserData } from "../services/githubApi";
+import { fetchUserData } from "../services/githubService";
 
 const Search = () => {
   const [username, setUsername] = useState("");
