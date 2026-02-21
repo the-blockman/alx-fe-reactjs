fdb6ddffb8f10b4e1f2f15af9bdd1a62d1b44df6
the-blockman <105639638+the-blockman@users.noreply.github.com>
2026-02-21 13:46:41 +0100

    add query client in file

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,10 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostsComponent from "./components/PostsComponent";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <PostsComponent />
-    </div>
+    </QueryClientProvider>
   );
 }
 
